19cf01bbaeb8a3cfb3e09b8020f7e15ec2d6ae20
Yohan Chayan <yohan.chayan7502@alumnos.udg.mx>
2022-11-25 11:46:42 -0600

    Logo navbar1

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -5,7 +5,7 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
-            {{-- {{ config('app.name', 'Laravel') }} --}}
+                <img class="img-fluid" width="50" height="50" src="{{asset('/custom/dashboard/img/favicon.png')}}" alt="logo">
             Control Escolar
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
