f46126f14d2645ee162d84f2f8472dfa8a751268
YohanChayan <yohan.chayan7502@alumnos.udg.mx>
2024-07-10 10:48:35 -0400

    remaining procedures seeder enabled

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
             CicloSeeder::class,
             UserSeeder::class,
             TramiteSeeder::class,
-            // TramiteSolicitadoSeeder::class,
+            TramiteSolicitadoSeeder::class,
         ]);
     }
 }
